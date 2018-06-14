11658b6f65a4499b494ddc83a97de8628f4c7e29
Michelle Seyler <michelle.m.seyler@gmail.com>
2018-06-14 22:34:59 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { 
+  'Brandi Carlile': "pride and joy",
+  'Pink': "I am here" 
+};
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[Whithey] = "I will always love you"
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[Pink] 
+  return playlist
+}
